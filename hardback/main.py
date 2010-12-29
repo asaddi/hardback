7f9fc08654b0c5093490809c77efd553a6892906
Allan Saddi <allan@saddi.com>
2010-12-28 21:28:57 -0800

    Far better command-line parsing.

--- hardback/main.py
+++ hardback/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-import getopt
+import argparse
 import hashlib
 import cStringIO as StringIO
 
@@ -12,29 +12,29 @@ WIDTH = 87
 BUF_SIZE = 1000 # Must be divisible by 5
 
 
-def enc_main():
+def enc_main(infile, outfile):
     h1 = hashlib.md5()
     h2 = hashlib.sha1()
     inp_len = 0
-    inp = sys.stdin.read(BUF_SIZE)
+    inp = infile.read(BUF_SIZE)
     out = StringIO.StringIO()
     while inp:
         h1.update(inp)
         h2.update(inp)
         inp_len += len(inp)
         out.write(encode(inp))
-        inp = sys.stdin.read(BUF_SIZE)
+        inp = infile.read(BUF_SIZE)
     out = out.getvalue()
 
     while out:
-        sys.stdout.write(out[:WIDTH] + '\n')
+        outfile.write(out[:WIDTH] + '\n')
         out = out[WIDTH:]
-    sys.stdout.write('# length: %s, alphabet: %s\n# md5: %s, sha1: %s\n' % (inp_len, ALPHA, h1.hexdigest(), h2.hexdigest()))
+    outfile.write('# length: %s, alphabet: %s\n# md5: %s, sha1: %s\n' % (inp_len, ALPHA, h1.hexdigest(), h2.hexdigest()))
 
 
-def dec_main(out_len):
+def dec_main(infile, outfile, out_len):
     inp = []
-    for line in sys.stdin:
+    for line in infile:
         line = line.strip()
         if line.startswith('#'): continue
         inp.append(line)
@@ -52,28 +52,24 @@ def dec_main(out_len):
     h1.update(out)
     h2.update(out)
 
-    sys.stdout.write(out)
+    outfile.write(out)
     sys.stderr.write('# md5: %s, sha1: %s\n' % (h1.hexdigest(), h2.hexdigest()))
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Hardcopy backup utility')
+    parser.add_argument('input', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help='input file')
+    parser.add_argument('-o', '--output', dest='output', type=argparse.FileType('w'), default=sys.stdout, help='output file')
+    parser.add_argument('-d', '--decode', dest='decode_len', type=int, help='decode with expected length DECODE_LEN')
+    args = parser.parse_args()
+
     try:
-        optlist, args = getopt.getopt(sys.argv[1:], 'd:')
-
-        do_decode = False
-        decode_len = 0
-        for opt, arg in optlist:
-            if opt == '-d':
-                do_decode = True
-                decode_len = int(arg)
-
-        if do_decode:
-            dec_main(decode_len)
+        if args.decode_len is not None:
+            dec_main(args.input, args.output, args.decode_len)
         else:
-            enc_main()
+            enc_main(args.input, args.output)
     except Exception as e:
-        sys.stderr.write(str(e) + '\n')
-        sys.exit(1)
+        sys.exit(str(e))
 
 
 if __name__ == '__main__':
